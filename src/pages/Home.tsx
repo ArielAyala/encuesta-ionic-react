338f20bd48027623917fe9cfeda32c10fe0ab5d8
Ariel Ayala <arielayalamail@gmail.com>
2020-03-15 20:30:21 -0300

    Initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Blank.</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Blank..</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
